264770a5d35271f50f3c5526f86d3d62c2aaeaff
DonXI <s6201012620082@email.kmutnb.ac.th>
2023-04-24 18:58:55 +0700

    edit unit test

--- unit_test_gui.py
+++ unit_test_gui.py
@@ -27,9 +27,30 @@ def test_checkBox_one():
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
-    # checkbox 
+    # checkbox hang_lueang
     ui.checkBox_hang_lueang.setChecked(True)
     assert ui.checkBox_hang_lueang.isChecked
+    # checkbox khang_pan
+    ui.checkBox_khang_pan.setChecked(True)
+    assert ui.checkBox_khang_pan.isChecked
+    # checkbox pod
+    ui.checkBox_pod.setChecked(True)
+    assert ui.checkBox_pod.isChecked
+    # checkbox ku_lare
+    ui.checkBox_ku_lare.setChecked(True)
+    assert ui.checkBox_ku_lare.isChecked
+    # checkbox too
+    ui.checkBox_too.setChecked(True)
+    assert ui.checkBox_too.isChecked
+    # checkbox see_kun
+    ui.checkBox_see_kun.setChecked(True)
+    assert ui.checkBox_see_kun.isChecked
+    # checkbox sai_dang
+    ui.checkBox_sai_dang.setChecked(True)
+    assert ui.checkBox_sai_dang.isChecked
+    # checkbox sai_dum
+    ui.checkBox_sai_dum.setChecked(True)
+    assert ui.checkBox_sai_dum.isChecked
 
 def test_button_start_with_checkBox():
     # open window
